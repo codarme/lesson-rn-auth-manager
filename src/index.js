b2ba2b4ee480eb4726819f347b20904abbc89011
Bruno Bertolini <dev@brunobertolini.com>
2019-11-11 20:29:55 -0300

    feat: auth handler

--- src/index.js
+++ src/index.js
@@ -1,8 +1,43 @@
 import * as React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, ActivityIndicator, TouchableOpacity } from 'react-native'
+import styled from 'styled-components'
+
+import { Login } from './Login'
+import { StoreProvider, useStore } from './store'
+
+const Loading = styled(ActivityIndicator)`
+  color: #fff;
+  flex: 1;
+`
+
+const Home = () => {
+  const [, setStore] = useStore()
+
+  const logout = () => {
+    setStore(({ auth, ...prev }) => prev)
+  }
+  return (
+    <View>
+      <Text>Home</Text>
+      <TouchableOpacity onPress={logout}>
+        <Text>Sair</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const Router = () => {
+  const [store] = useStore()
+
+  if (!store.rehydrated) {
+    return <Loading />
+  }
+
+  return store.auth ? <Home /> : <Login />
+}
 
 export const App = () => (
-  <View>
-    <Text>App</Text>
-  </View>
+  <StoreProvider>
+    <Router />
+  </StoreProvider>
 )
