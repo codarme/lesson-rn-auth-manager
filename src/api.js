a709540416d6eece17f02fe97ca89390185f9b99
Bruno Bertolini <dev@brunobertolini.com>
2019-11-11 20:28:42 -0300

    feat: add mocked api

---
+++ src/api.js
@@ -0,0 +1,16 @@
+export const login = credentials =>
+  new Promise(resolve => {
+    setTimeout(
+      () =>
+        resolve({
+          data: {
+            token: 123456,
+            user: {
+              name: 'Bruno',
+              email: 'dev@brunobertolini.com',
+            },
+          },
+        }),
+      2000
+    )
+  })
